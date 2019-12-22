8abdc143662c6b0fbe4393fd365396252e18df19
yozian <yozian@hotmail.com>
2019-12-22 14:17:10 +0800

    add stringBuilder extensions

---
+++ src/Yozian.Extension.Test/StringBuilderExtensionTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using Yozian.Extension.Test.TestMaterial;
+
+namespace Yozian.Extension.Test
+{
+    public class StringBuilderExtensionTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase()]
+        public void Test_AppendWhenFalse()
+        {
+            var sb = new StringBuilder();
+            var template = "won't be concated!";
+            sb.AppendWhen(false, template);
+
+            Assert.AreEqual(string.Empty, sb.ToString());
+        }
+
+        [TestCase()]
+        public void Test_AppendWhenTrue()
+        {
+            var sb = new StringBuilder();
+            var template = "it's me!";
+            sb.AppendWhen(true, template);
+
+            Assert.AreEqual(template, sb.ToString());
+        }
+
+        [TestCase()]
+        public void Test_AppendWhenFalseWithArgs()
+        {
+            var sb = new StringBuilder();
+            var template = "won't be concated!{0},{1}";
+            sb.AppendWhen(false, template, "a", "b");
+
+            Assert.AreEqual(string.Empty, sb.ToString());
+        }
+
+        [TestCase()]
+        public void Test_AppendWhenTrueWithArgs()
+        {
+            var sb = new StringBuilder();
+            var template = "hi, {0},{1}";
+            sb.AppendWhen(true, template, "a", "b");
+
+            Assert.AreEqual(string.Format(template, "a", "b"), sb.ToString());
+        }
+    }
+}
---
+++ src/Yozian.Extension/StringBuilderExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yozian.Extension
+{
+    public static class StringBuilderExtension
+    {
+        public static StringBuilder AppendWhen(this StringBuilder @this, bool matched, string text)
+        {
+            if (matched)
+            {
+                @this.Append(text);
+            }
+
+            return @this;
+        }
+
+        public static StringBuilder AppendWhen(
+            this StringBuilder @this,
+            bool matched,
+            string text,
+            params string[] values
+        )
+        {
+            if (matched)
+            {
+                @this.Append(string.Format(text, values));
+            }
+
+            return @this;
+        }
+
+        public static StringBuilder AppendLineWhen(
+            this StringBuilder @this,
+            bool matched,
+            string text
+        )
+        {
+            if (matched)
+            {
+                @this.AppendLine(text);
+            }
+
+            return @this;
+        }
+
+        public static StringBuilder AppendLineWhen(
+            this StringBuilder @this,
+            bool matched,
+            string text,
+            params string[] values
+        )
+        {
+            if (matched)
+            {
+                @this.AppendLine(string.Format(text, values));
+            }
+
+            return @this;
+        }
+    }
+}
