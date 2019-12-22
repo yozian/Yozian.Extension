ce04d88ab4cc5d57e7a6f60de7b5f176090c8420
yozian <yozian@hotmail.com>
2019-12-22 14:44:17 +0800

    fix build output path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 dotnet publish src/Yozian.Extension/Yozian.Extension.csproj \
     --force \
     -c Release \
-    -o "../../nuget/lib/netstandard2.0"
+    -o "nuget/lib/netstandard2.0"
     
 rm nuget/lib/netstandard2.0/Yozian.Extension.deps.json
