b1cb6072a449e6a3d78ed0ec84c80e3e6fd431f9
yozian <yozian@hotmail.com>
2021-03-12 02:00:14 +0800

    implement gernericComparer

--- src/Yozian.Extension.Test/ICollectionExtensionTest.cs
+++ src/Yozian.Extension.Test/ICollectionExtensionTest.cs
@@ -1,22 +1,21 @@
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using Yozian.Extension.CollectionDto;
 
 namespace Yozian.Extension.Test
 {
     public class ICollectionExtensionTest
     {
-
-
         [SetUp]
         public void Setup()
         {
-
         }
 
         [TestCase()]
         public void Test_AddWhen()
         {
-            var collection = new List<int>() { 1, 2, 3 };
+            var collection = new List<int>() {1, 2, 3};
 
             collection.AddWhen(false, 4);
 
@@ -25,13 +24,12 @@ namespace Yozian.Extension.Test
             collection.AddWhen(true, 4);
 
             Assert.AreEqual(4, collection.Count);
-
         }
 
         [TestCase()]
-        public void RemoveWhen()
+        public void Test_RemoveWhen()
         {
-            var collection = new List<int>() { 1, 2, 3 };
+            var collection = new List<int>() {1, 2, 3};
 
             collection.RemoveWhen(false, 1);
 
@@ -40,6 +38,35 @@ namespace Yozian.Extension.Test
             collection.RemoveWhen(true, 1);
 
             Assert.AreEqual(2, collection.Count);
+        }
+
+
+        [TestCase()]
+        public void Test_Comparer()
+        {
+            var sourceA = Enumerable
+                .Range(1, 7)
+                .Select(i => new KeyValuePair<string, int>(i.ToString(), i))
+                .ToList();
+
+
+            var sourceB = Enumerable
+                .Range(1, 5)
+                .Select(i => new KeyValuePair<string, int>(i.ToString(), i))
+                .ToList();
+
+
+            var excludedList = sourceA
+                .Except(
+                    sourceB,
+                    new GenericComparer<KeyValuePair<string, int>>((x, y) => x.Key == y.Key)
+                )
+                .ToList();
+
+
+            Assert.AreEqual(2, excludedList.Count);
+
+            Assert.AreEqual("6,7", string.Join(",", excludedList.Select(x => x.Key)));
         }
     }
 }
---
+++ src/Yozian.Extension/CollectionDto/GenericComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yozian.Extension.CollectionDto
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class GenericComparer<T> : IEqualityComparer<T>
+        where T : new()
+    {
+        private readonly Func<T, T, bool> compareMethod;
+
+        public GenericComparer(Func<T, T, bool> compareMethod)
+        {
+            this.compareMethod = compareMethod;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return this.compareMethod(x, y);
+        }
+
+        public virtual int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
