a5b6f08bd8390e7edc34e10c64e612c391b0e669
yozian <yozian@hotmail.com>
2024-11-08 15:38:29 +0800

    feat: add Guid Extension

---
+++ src/Yozian.Extension.Test/GuidExtensionTest.cs
@@ -0,0 +1,52 @@
+using System;
+using NUnit.Framework;
+
+namespace Yozian.Extension.Test;
+
+[TestFixture]
+public class GuidExtensionTest
+{
+    [Test]
+    public void Test_IsNullOrEmpty()
+    {
+        Guid? guid = null;
+
+        Assert.IsTrue(guid.IsNullOrEmpty());
+
+        guid = Guid.Empty;
+
+        Assert.IsTrue(guid.IsNullOrEmpty());
+
+        guid = Guid.NewGuid();
+
+        Assert.IsFalse(guid.IsNullOrEmpty());
+    }
+
+    [Test]
+    public void Test_IsEmpty()
+    {
+        var guid = Guid.Empty;
+
+        Assert.IsTrue(guid.IsEmpty());
+
+        guid = Guid.NewGuid();
+
+        Assert.IsFalse(guid.IsEmpty());
+    }
+
+    [Test]
+    public void Test_Increment()
+    {
+        var guid = Guid.NewGuid();
+
+        // increment 1000 times, make it sure it won't overflow
+        for (int i = 0; i < 1000; i++)
+        {
+            var incrementedGuid = guid.Increment();
+
+            Assert.AreNotEqual(guid, incrementedGuid);
+            //
+            guid = incrementedGuid;
+        }
+    }
+}
---
+++ src/Yozian.Extension/GuidExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Yozian.Extension;
+
+/// <summary>
+/// 
+/// </summary>
+public static class GuidExtension
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsNullOrEmpty(this Guid? @this)
+    {
+        return null == @this || @this.Value.IsEmpty();
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsEmpty(this Guid @this)
+    {
+        return @this == Guid.Empty;
+    }
+
+    public static Guid Increment(this Guid @this)
+    {
+        var bytes = @this.ToByteArray();
+
+        for (int i = bytes.Length - 1; i >= 0; i--)
+        {
+            if (++bytes[i] != 0)
+            {
+                break;
+            }
+        }
+
+        return new Guid(bytes);
+    }
+}
