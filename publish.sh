1cb6900843154ed2d211bba75963941289de53cd
yozian <yozian@hotmail.com>
2025-11-29 02:09:37 +0800

    feat: add run.sh script for build, pack, and publish commands; remove obsolete pack.sh and publish.sh scripts

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-version=$1
-
-if [ "$version" == "" ];then
-   echo "version should be provided!"
-   exit;
-fi
-
-dotnet nuget push "nuget/Yozian.Extension.$version.nupkg" --source https://api.nuget.org/v3/index.json --api-key $NUGET_API_KEY
